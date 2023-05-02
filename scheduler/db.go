02cd6190315f428797efbf522fb3be2ea4ecfc6a
skrider <skrider@berkeley.edu>
2023-05-01 23:43:42 -0700

    break out db

---
+++ scheduler/db.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"sync"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+var once sync.Once
+var mutex sync.Mutex
+
+func GetDB() *sql.DB {
+    once.Do(func() {
+        var err error
+
+        db, err = connect()
+        if err != nil {
+            panic(err)
+        }
+
+        err = prepare(db)
+        if err != nil {
+            panic(err)
+        }
+
+        for i := 0; i < 30; i++ {
+            err := db.Ping()
+            if err == nil {
+                break
+            }
+            time.Sleep(time.Second)
+        }
+    })
+    if db == nil {
+        panic("db is nil")
+    }
+    return db
+}
+
+func connect() (*sql.DB, error) {
+	bin, err := ioutil.ReadFile("/run/secrets/db-password")
+	if err != nil {
+		return nil, err
+	}
+	url := fmt.Sprintf("postgres://postgres:%s@db:5432/example?sslmode=disable", strings.Trim(string(bin), "\n"))
+	return sql.Open("postgres", url)
+}
+
+func prepare(db *sql.DB) error {
+	if _, err := db.Exec("DROP TABLE IF EXISTS blog"); err != nil {
+		return err
+	}
+
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS blog (id SERIAL, title VARCHAR)"); err != nil {
+		return err
+	}
+
+	for i := 0; i < 5; i++ {
+		if _, err := db.Exec("INSERT INTO blog (title) VALUES ($1);", fmt.Sprintf("Blog post #%d", i)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
--- scheduler/go.mod
+++ scheduler/go.mod
@@ -3,19 +3,16 @@ module github.com/skrider/serverless-model-example/scheduler
 go 1.18
 
 require (
-	github.com/gorilla/handlers v1.3.0
-	github.com/gorilla/mux v1.6.2
+	github.com/docker/docker v23.0.5+incompatible
+	github.com/docker/go-connections v0.4.0
 	github.com/lib/pq v1.10.3
 )
 
 require (
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
-	github.com/docker/docker v23.0.5+incompatible // indirect
-	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/gorilla/context v1.1.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
