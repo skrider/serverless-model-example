41eec0c25248b0608479dda2f1f8e4d667d0e0a5
skrider <skrider@berkeley.edu>
2023-05-01 19:05:15 -0700

    add /ok endpoint to replcia

--- model/server.py
+++ model/server.py
@@ -13,5 +13,9 @@ def predict():
     input = request.json['input']
     return model.predict(input)
 
+@app.route('/ok')
+def ok():
+    return 'ok'
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
